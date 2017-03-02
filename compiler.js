37734f13e8a7ababfababbd79714b3f8a6a90d43
Mihai Bazon <mihai.bazon@gmail.com>
2017-03-02 18:45:18 +0200

    support passing the runtime as an option to `compile`

--- compiler.js
+++ compiler.js
@@ -14,7 +14,7 @@ function compile(files, options) {
     var paths = option("paths", {});
     var base = option("base", null);
     var beautify = option("beautify", false);
-    var runtime = TWEEG_RUNTIME();
+    var runtime = option("runtime", TWEEG_RUNTIME());
     var tweeg = TWEEG(runtime).init();
 
     var code = "";
